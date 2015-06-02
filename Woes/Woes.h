98a3ced3abf76901785b41a75436511e8a171089
Zak Remer <zak.remer@gmail.com>
2015-06-02 00:00:52 -0700

    Adds xcodeproj.

---
+++ Woes/Woes.h
@@ -0,0 +1,19 @@
+//
+//  Woes.h
+//  Woes
+//
+//  Created by Zak Remer on 6/1/15.
+//  Copyright (c) 2015 Zak Remer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Woes.
+FOUNDATION_EXPORT double WoesVersionNumber;
+
+//! Project version string for Woes.
+FOUNDATION_EXPORT const unsigned char WoesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Woes/PublicHeader.h>
+
+
